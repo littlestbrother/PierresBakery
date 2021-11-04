c054ae0feed08b6563c753304ae8363021921af3
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-11-04 15:09:10 -0700

    add necessary models

---
+++ Models/Flavor.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models {
+  public class Flavor {
+    public Flavor() {
+      this.JoinEntities = new HashSet <FlavorTreat> ();
+    }
+
+    public int FlavorId {get; set;}
+    public string Name {get; set;}
+    public virtual ICollection <FlavorTreat> JoinEntities {get; set;}
+  }
+}
